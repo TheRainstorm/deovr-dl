3a729853eec417be9a941550fbbf7096f8581a5c
Fuyan Yuan <33221728+TheRainstorm@users.noreply.github.com>
2024-08-08 00:15:28 +0800

    ✨ feat: re-implement multi-threading suppporting recover from failed

--- deovr-dl.py
+++ deovr-dl.py
@@ -1,7 +1,6 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import math
-import os
 import threading
+import queue
+import os
 import time
 import requests
 import argparse
@@ -36,10 +35,11 @@ def download_chunk_helper(url, start, end, stream=True):
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0'
     }
+    end = '' if end == -1 else end
     range_header = f'bytes={start}-{end}'
     chunk_headers = headers.copy()
     chunk_headers['Range'] = range_header
-    response = requests.get(url, headers=chunk_headers, stream=stream)
+    response = requests.get(url, headers=chunk_headers, stream=stream, timeout=(10, 5))  # set timeout, so don't hang long time
     return response
 
 def print_speed(seconds, total_size):
@@ -59,42 +59,117 @@ def download_file(url, output_file='output.mp4'):
         total_size = f.tell()
     print_speed(time.time() - tic, total_size)
 
-def download_file_in_chunks(url, start_offset=64, chunk_size=100 * 1024 * 1024, output_file='output.mp4', max_threads=4):
+stop_event = threading.Event()
+def download_chunk(tid, result_queue, shared_data, lock, task_queue):
+    while not stop_event.is_set():
+        with lock:
+            if task_queue.empty():
+                break
+            chunk_id, start, end = task_queue.get()
+        print(f"{f'Thread {tid}: Downloading chunck':<30s} {chunk_id:3d}/{shared_data['chunk_num']:<3d}")
+        
+        try:
+            response = download_chunk_helper(shared_data['url'], start, end)
+            if response.status_code == 206:
+                result_queue.put((tid, chunk_id, start, response.content))
+            else:
+                print(f'Thread {tid} Error: HTTP response code {response.status_code}, downloading {start:,}-{end:,}')
+                result_queue.put((tid, chunk_id, -2, None))
+        except Exception as e:
+            print(f'Thread {tid} Exception {e}, downloading {start:,}-{end:,}')
+            result_queue.put((tid, chunk_id, -2, None))
+        
+    result_queue.put((tid, -1, -1, None))
+    print(f'Thread {tid} finished')
+
+def download_file_in_chunks(url, start_offset=64, chunk_size=100 * 1024 * 1024, output_file='output.mp4', recover_file="", max_threads=4):
     tic = time.time()
-    def download_chunk(url, chunk_id, start, end):
-        thread_id = threading.current_thread().name.split('_')[-1]
-        thread_name = f'Thread {thread_id}'
-        print(f'{f"{thread_name} Downloading chunk":30s} {chunk_id:3d}/{chunk_num:<3d}')
-        
-        response = download_chunk_helper(url, start, end, stream=False)
-        if response.status_code != 206:
-            return -1, -1, None
-        return chunk_id, start, response
+    recover_mode = False
+    task_finished = []
+    if recover_file and os.path.exists(recover_file):
+        with open(recover_file, 'r') as f:
+            task_finished = json.load(f)
+            recover_mode = True
     
-    with open(output_file, 'wb') as f:
-        # get video file total size
-        response = download_chunk_helper(url, 0, start_offset-1)
-        total_size = int(response.headers.get('Content-Range').split('/')[-1])
-        f.write(response.content)
+    out_file = open(output_file, 'wb' if not recover_mode else 'r+b')
+    # get total size
+    response = download_chunk_helper(url, 0, start_offset-1)
+    total_size = int(response.headers.get('Content-Range').split('/')[-1])
+    out_file.write(response.content)
     
-        futures = []
-        chunk_num = math.ceil((total_size - start_offset) / chunk_size)
-        with ThreadPoolExecutor(max_threads) as executor:
-            for start in range(start_offset, total_size, chunk_size):
-                end = min(start + chunk_size - 1, total_size - 1)
-                chunk_id = len(futures)
-                futures.append(executor.submit(download_chunk, url, chunk_id, start, end))
+    lock = threading.Lock()
+    task_queue = queue.Queue()
+    chunk_id = 0
+    for start in range(start_offset, total_size, chunk_size):
+        end = min(start + chunk_size - 1, total_size - 1)
+        if chunk_id not in task_finished:  # not downloaded
+            task_queue.put((chunk_id, start, end))
+        chunk_id += 1
+    chunk_num = chunk_id
+    
+    if recover_mode:
+        remain = task_queue.qsize()/chunk_num
+        print(f"Recovered {1 - remain:.2f}, remain {remain:.2f}")
+
+    shared_data = {
+        'url': url,
+        'chunk_num': chunk_num,
+    }
+    
+    result_queue = queue.Queue()
+    threads = []
+    for i in range(max_threads):
+        t = threading.Thread(target=download_chunk, args=(i, result_queue, shared_data, lock, task_queue))
+        t.start()
+        threads.append(t)
+    
+    success = True
+    count_finished = 0
+    download_bytes = 0
+    try:
+        while True:
+            tid, chunk_id, start, chunk = result_queue.get()
+            if start == -1:  # download finished
+                count_finished += 1
+                if count_finished == max_threads:
+                    break
+                continue
+            elif start == -2:  # download error
+                success = False
+                continue
+            print(f'{"         Downloaded chunk":<30s} {chunk_id:3d}')
+            out_file.seek(start)
+            out_file.write(chunk)
+            download_bytes += len(chunk)
+            task_finished.append(chunk_id)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt, saving recover file")
+        print(task_finished)
+        task_finished.sort()
+        with open(recover_file, 'w') as f:
+            json.dump(task_finished, f, indent=4)
         
-            for future in as_completed(futures):
-                chunk_id, start, response = future.result()
-                print(f'{"         Downloaded chunk":<30s} {chunk_id:3d}')
-                if response:
-                    f.seek(start)
-                    f.write(response.content)
-                else:
-                    print('Failed to download chunk')
-                    exit(-1)
-    print_speed(time.time() - tic, total_size)
+        out_file.close()
+        
+        stop_event.set()
+        for t in threads:
+            t.join()
+        print("All threads stopped, exit")
+        exit(0)
+    
+    out_file.close()
+    if not success:
+        with open(recover_file, 'w') as f:
+            json.dump(task_finished, f, indent=4)
+    else:
+        if os.path.exists(recover_file):
+            os.remove(recover_file)
+    
+    toc = time.time()
+    speed = download_bytes / (toc - tic)
+    print(f"Download bytes: {download_bytes:,}|{download_bytes/1024**2:.2f} MiB")
+    print(f"Elapsed time: {seconds_to_hms(int(toc - tic))} Speed: {speed/1024**2:.2f} MiB/s")
+    return success
 
 def seconds_to_hms(seconds):
     hours = seconds // 3600
@@ -117,7 +192,7 @@ parser.add_argument('-c', '--encoding', nargs='+', default='h264', help='filter 
 parser.add_argument('-f', '--select-format-idx', type=int, help='select format by index. If not set, select the best quality with filted encoding')
 
 parser.add_argument('-n', '--thread-number', type=int, default=0, help='parallel download threads, 0 for original downloader')
-parser.add_argument('-C', '--chunck-size', type=int,  default=20*1024**2, help='Download in chunks of n bytes, default 20 MiB')
+parser.add_argument('-C', '--chunk-size', type=int,  default=20*1024**2, help='Download in chunks of n bytes, default 20 MiB')
 args = parser.parse_args()
 
 # get video data
@@ -149,7 +224,7 @@ print(f"\n**** Download Param:")
 print(f"** Select encoding: {selected_src['encoding']}")
 print(f"** Selected quality: {selected_src['quality']}")
 if args.thread_number!=0:
-    print(f"** Chunk size {args.chunck_size/1024**2:.2f} MiB")
+    print(f"** Chunk size {args.chunk_size/1024**2:.2f} MiB")
     print(f"** Threads: {args.thread_number}")
 
 # download url
@@ -160,6 +235,8 @@ if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir, exist_ok=True)
 filename = f"{args.title} - {selected_src['encoding']} {selected_src['quality']}"
 output_file = os.path.join(args.output_dir, f"{filename}.mp4")
+output_tmp_file = os.path.join(args.output_dir, f"{filename}.mp4.tmp")
+recover_file = os.path.join(args.output_dir, f"{filename}.recover.json")
 
 print(f"Download to: {output_file}")
 if os.path.exists(output_file):
@@ -171,4 +248,10 @@ if os.path.exists(output_file):
 if args.thread_number == 0:
     download_file(selected_src['url'], output_file)
 else:
-    download_file_in_chunks(selected_src['url'], output_file=output_file, max_threads=args.thread_number, chunk_size=args.chunck_size)
+    success = download_file_in_chunks(selected_src['url'], output_file=output_tmp_file, recover_file=recover_file, max_threads=args.thread_number, chunk_size=args.chunk_size)
+    if success:
+        print('Download successed')
+        os.rename(output_tmp_file, output_file)
+        print(f"File size: {os.path.getsize(output_file):,} bytes")
+    else:
+        print('Download failed, run again to recover')
